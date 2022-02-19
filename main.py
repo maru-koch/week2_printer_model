b502dc6cac7b104caa1a09b095fd84477b166e46
maru_koch <nwokochafranklyn@gmail.com>
2022-02-19 15:05:57 +0100

    added a constructor
    Added a constructor to the Printer class and initialized the ink, and paper variables

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 # The entry point of your application
-class Main():
-    def print():
-        pass
+from modules.printer import Printer
+
+class Operation():
+    on = False
+
+    def start():
+        on = True
+        while on:
+            response = Printer.start()
+            if response is True:
+                print(Printer.generateReport())
+                Printer.printDocument()
+                print(Printer.generateReport())
+            else:
+                Printer.rejectDocument()
+            
+    def checkResource():
+        Printer.checkResource()
+
+    def stop(self):
+        self.on = False
+
     
+    
+
+
+        
+if __name__== "__main__" :
+    Operation.start()
--- modules/printer.py
+++ modules/printer.py
@@ -1,6 +1,24 @@
-from data.data import resources, FORMAT, coins
+
+from data.data import FORMAT, resources, coins
 
 class Printer():
+    coin = ''
+    pages = 0
+    mode = ''
+    number = 0
+
+    def __init__(self):
+        self.ink = resources['ink']
+        self.paper = resources['paper']
+
+    def start(self):
+        self.mode = input("What format would you like? greyscale or coloured: ")
+        self.pages = int(input("How many pages would like to print: "))
+        self.coin = input("Please Insert coin... (Penny, Nickel, Dime, or Quarter): ").islower()
+        self.number = int(input("Enter Number of coins: "))
+        response = Printer.checkAvailableResources(self.mode, self.pages)
+        return response
+
     def checkMode(self, mode):
         if mode == 'coloured':
             print_format = FORMAT['coloured']
@@ -8,18 +26,58 @@ class Printer():
             print_format = FORMAT['greyscaled']
         return print_format
 
+    def printDocument():
+        resources['ink'] - ink 
+        resources['paper'] - paper 
+        Printer.increaseProfit()
+    
+    def rejectDocument():
+        print("Document rejected")
+
     def checkResource(self, mode, paper, ink, pages):
         pass
 
-    def processPrice(pages, coin):
-        price = coin * pages
-        return f"Your price is {price}"
+    def processPrice(pages, mode, coin, number) -> int:
+        cost = FORMAT[mode]['price'] * pages
+        amountEntered = coins[coin] * number
+        return cost, amountEntered
 
-    def checkTransaction(self, coins, cost):
-        if cost > coins:
+    def checkTransaction():
+        cost, amountEntered = Printer.processPrice()
+        if cost > amountEntered:
             return "Insufficient funds"
-        elif cost == coins:
-            pass
+        elif amountEntered == cost:
+            return True
         else:
-            balance = coins - cost
-            return f"Your balance is {balance}"
+            Printer.balance(amountEntered, cost)
+            return True
+
+    def balance(amountEntered, cost):
+        balance = amountEntered - cost
+        return f"Here is ${balance} in change"
+
+    def checkAvailableResources(mode, pages):
+        """
+            - Check if there is sufficient amount of ink and paper
+            - if not raise error
+        """
+        available_ink = resources['ink']
+        avalaible_paper = resources['paper']
+        ink = FORMAT[mode]['materials']['ink'] * pages
+        if ink > available_ink or pages > avalaible_paper:
+            raise InsufficientResourcesException
+        else:
+            return True
+
+    def increaseProfit(self, price: int) -> int:
+        resources['profit'] += price
+         
+    def generateReport() -> str:
+        report = f"Paper: {resources['paper']} \nInk: {resources['ink']} \nProfit: {resources['profit']}"
+        return report
+    
+    def exitMsg(self):
+        return "Here is your report. Thank you for using our services"
+
+class InsufficientResourcesException(Exception):
+    pass
